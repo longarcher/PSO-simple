59ce1b25dedb4f6d5922d455e327662b7b56adfa
longarcher <61976084+longarcher@users.noreply.github.com>
2023-04-17 22:05:17 -0400

    Update and rename temp to loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
-// @name         MBS - Maid's Bondage Scripts
-// @namespace    MBS
-// @version      0.5.10
-// @description  Loader of Bananarama92's "Maid's Bondage Scripts" mod
-// @author       Bananarama92
+// @name         PSO command addon
+// @namespace    PSO-simple
+// @version      0.1
+// @description  Loader simple PSO command addon
+// @author       Sara
 // @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
-// @homepage     https://github.com/bananarama92/MBS.git#readme
-// @source       https://github.com/bananarama92/MBS.git
-// @downloadURL  https://github.com/bananarama92/MBS/raw/main/src/loader.user.js
+// @source       https://github.com/longarcher/PSO-simple.git
+// @downloadURL  https://github.com/longarcher/PSO-simple/raw/main/src/loader.user.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
@@ -19,7 +18,7 @@ setTimeout(
         const n = document.createElement("script");
         n.language = "JavaScript";
         n.crossorigin = "anonymous";
-        n.src = "https://bananarama92.github.io/MBS/main/mbs.js";
+        n.src = "https://github.com/longarcher/PSO-simple/main/loader.user.js;
         document.head.appendChild(n);
     },
     2000,
